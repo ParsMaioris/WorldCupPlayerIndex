85c0a29f24fd8c65c43ec688b3946a726924b357
InnovateWithRK <rk.go@icloud.com>
2025-02-10 13:57:36 -0800

    remove FileLogger and related options; eliminate unused logging implementation

--- src/Infrastructure/CrossCutting/Logging/FileLoggerOptions.cs
+++
@@ -1,7 +0,0 @@
-public class FileLoggerOptions
-{
-    public string LogDirectory { get; set; } = "logs";
-    public string FileNamePrefix { get; set; } = "app";
-    public int RetentionDays { get; set; } = 7;
-    public LogLevel MinimumLogLevel { get; set; } = LogLevel.Warning;
-}
